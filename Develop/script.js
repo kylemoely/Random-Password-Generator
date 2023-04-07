24da24ef5c48e6628694a9488b0f8a08691c3e35
kmoely <kylemoely@gmail.com>
2023-04-06 21:09:56 -0500

    Added some error messages for invalid inputs

--- Develop/script.js
+++ Develop/script.js
@@ -11,28 +11,51 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var upLets = prompt("Would you like the password to include upper case letters (Y/N)?");
+    if(upLets===null){
+      return;
+    }
     if(upLets!="Y" && upLets!="N"){
     alert("Invalid answer. Please enter Y or N.")
     return;
   }
   var downLets = prompt("Would you like the password to include lower case letters (Y/N)?");
+  if(downLets===null){
+    return;
+  }
   if(downLets!="Y" && downLets!="N"){
     alert("Invalid answer. Please enter Y or N.")
     return;
   }
   var specs = prompt("Would you like the password to include special characters (Y/N)?");
+  if(specs===null){
+    return;
+  }
   if(specs!="Y" && specs!="N"){
     alert("Invalid answer. Please enter Y or N.")
     return;
   }
   var nums = prompt("Would you like the password to include numbers (Y/N)?");
+  if(nums===null){
+    return;
+  }
   if(nums!="Y" && nums!="N"){
     alert("Invalid answer. Please enter Y or N.")
     return;
   }
+  if(upLets==="N" && downLets==="N" && specs==="N" && nums==="N"){
+    alert("Invalid. Your password must contain characters.")
+    return;
+  }
   var passLength = prompt("How many characters would you like the password to be (Minimum: 8, Maximum: 128)?")
+  if(passLength===null){
+    return;
+  }
+  // if(typeof passLength!="number"){
+  //   alert("Invalid answer. Please enter a number.")
+  //   return;
   if(passLength>128||passLength<8){
     alert("Invalid answer. Password length must be between 8 and 128 characters.")
+    return;
   }
 
   var options = [];
@@ -63,7 +86,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
